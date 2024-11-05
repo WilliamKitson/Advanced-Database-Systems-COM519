6dc79c3bcce2b8dfd54027797ec2054a145a833c
4kitsw10 <4kitsw10@solent.ac.uk>
2024-11-05 11:28:19 +0000

    main redefined

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import tkinter
+import sqlite3
+from tkinter import ttk
 
 window = tkinter.Tk()
-
 window.title("4kitsw10 COM519")
-window.geometry("1280x720")
+
+treeview = ttk.Treeview(columns=("size", "lastmod"))
+treeview.heading("#0", text="Name")
+treeview.heading("size", text="RSP")
+treeview.heading("lastmod", text="Calories")
+
+connection_obj = sqlite3.connect("Restaurant_Management_System")
+cursor = connection_obj.cursor()
+
+cursor.execute(f"SELECT * FROM Customer_Facing_Menu")
+
+for row in cursor.fetchall():
+    treeview.insert(
+        "",
+        tkinter.END,
+        text=row[1],
+        values=(row[2], row[3])
+    )
+
+treeview.pack()
 window.mainloop()
