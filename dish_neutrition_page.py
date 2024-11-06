39ba19baf24ff1c820011fda14420d5bc3410a9d
4kitsw10 <4kitsw10@solent.ac.uk>
2024-11-06 09:56:22 +0000

    DishNutritionPage redefined

--- dish_neutrition_page.py
+++ dish_neutrition_page.py
@@ -1,10 +1,27 @@
 import sqlite3
+from tkinter import ttk
 
 class DishNutritionPage:
     def __init__(self, window, dish):
         self.__window = window
+        self.__treeview = self.__initialise_treeview()
         self.__cursor = self.__initialise_cursor()
         self.__dish = dish
+
+    def __initialise_treeview(self):
+        headers = (
+            'Ingredient',
+            'Quantity',
+            'Total Weight',
+            'Total Calories'
+        )
+
+        tree = ttk.Treeview(self.__window, columns=headers, show='headings')
+
+        for i in headers:
+            tree.heading(i, text=i)
+
+        return tree
 
     @staticmethod
     def __initialise_cursor():
@@ -20,4 +37,11 @@ class DishNutritionPage:
         )
 
         for row in self.__cursor.fetchall():
-            print(row)
+            self.__treeview.insert("", "end", values=(
+                row[1],
+                row[2],
+                f"{row[3]}g",
+                f"{row[4]} cals"
+            ))
+
+            self.__treeview.pack(fill="x")
