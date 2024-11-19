eba898fbe6e508a8dce861ea69c64ba701846bdf
4kitsw10 <4kitsw10@solent.ac.uk>
2024-11-19 14:18:01 +0000

    login page redefined

--- login_page.py
+++ login_page.py
@@ -1,13 +1,27 @@
-import sqlite3
 import tkinter
-from tkinter import ttk
+from tkinter import *
+import sqlite3
 
 class LoginPage:
     def __init__(self, database):
-        self._database = database
-        self._cursor = sqlite3.connect(database).cursor()
-        self._window = tkinter.Tk()
-        self._window.title("login")
+        self.__database = database
+        self.__cursor = sqlite3.connect(database).cursor()
+        self.__window = tkinter.Tk()
+        self.__window.title("login")
+        self.__window.geometry('400x250')
 
     def render(self):
-        self._window.mainloop()
+        Label(self.__window, text="Username").grid(row=0, column=0)
+        username = StringVar()
+        Entry(self.__window, textvariable=username).grid(row=0, column=1)
+
+        Label(self.__window, text="Password").grid(row=1, column=0)
+        password = StringVar()
+        Entry(self.__window, textvariable=password, show='*').grid(row=1, column=1)
+
+        Button(self.__window, text="Login", command="self.__login").grid(row=3, column=0)
+
+        self.__window.mainloop()
+
+    def __login(self, username, password):
+        print("test")
