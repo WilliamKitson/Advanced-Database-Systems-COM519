2343ac6f3ca3f9a1248d6b68fbd993a8efdf5c43
4kitsw10 <4kitsw10@solent.ac.uk>
2024-11-19 14:32:55 +0000

    login page redefined

--- login_page.py
+++ login_page.py
@@ -16,22 +16,20 @@ class LoginPage:
     def render(self):
         self.__render_username()
         self.__render_password()
-
-        command_login = partial(self.__login, self.__username, self.__password)
-        Button(self.__window, text="Login", command=command_login).grid(row=3, column=0)
-
-
+        self.__render_login()
         self.__window.mainloop()
 
     def __render_username(self):
         Label(self.__window, text="Username").grid(row=0, column=0)
-
         Entry(self.__window, textvariable=self.__username).grid(row=0, column=1)
 
     def __render_password(self):
         Label(self.__window, text="Password").grid(row=1, column=0)
-
         Entry(self.__window, textvariable=self.__password, show='*').grid(row=1, column=1)
+
+    def __render_login(self):
+        command_login = partial(self.__login, self.__username, self.__password)
+        Button(self.__window, text="Login", command=command_login).grid(row=3, column=0)
 
     def __login(self, username, password):
         print("test")
