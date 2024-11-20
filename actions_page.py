8bba0464d06f626ecd86a996ea27ef3bb510f335
4kitsw10 <4kitsw10@solent.ac.uk>
2024-11-20 11:27:51 +0000

    actions_page redefined

--- actions_page.py
+++ actions_page.py
@@ -3,10 +3,13 @@ from treeview_page import TreeviewPage
 class ActionsPage(TreeviewPage):
     def __init__(self, database):
         columns = [
-            "Action"
+            "Action",
+            "Description"
         ]
 
         TreeviewPage.__init__(self, database, "Actions Page", columns)
 
     def render(self):
+        self._treeview.insert("", "end", values=("View Staff", "BLABLABLA"))
+        self._treeview.insert("", "end", values=("View Menu", "BLABLABLA"))
         self._treeview.grid(row=0, column=0)
