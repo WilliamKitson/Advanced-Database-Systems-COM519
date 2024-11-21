3dcca33d6e2fde53bb242959226fc3427cde0bf4
4kitsw10 <4kitsw10@solent.ac.uk>
2024-11-21 11:50:21 +0000

    treeview_frame redefined

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from treeview_frame import TreeviewFrame
 window = tkinter.Tk()
 window.title("4kitsw10_COM519")
 
-treeview_one = TreeviewFrame("4kitsw10_COM519_database", window, ["one", "two", "three"])
+treeview_one = TreeviewFrame("4kitsw10_COM519_database", window)
 treeview_one.render()
 treeview_one.clear()
 
-TreeviewFrame("4kitsw10_COM519_database", window, ["four", "five", "six"]).render(window)
+TreeviewFrame("4kitsw10_COM519_database", window).render()
 
 window.mainloop()
--- treeview_frame.py
+++ treeview_frame.py
@@ -3,21 +3,29 @@ from tkinter import *
 from tkinter import ttk
 
 class TreeviewFrame:
-    def __init__(self, database, window, columns):
-        self._database = database
-        self._cursor = sqlite3.connect(database).cursor()
-        self._columns = columns
+    def __init__(self, database, window):
         self.__frame = Frame(window)
 
     def render(self):
-        treeview = ttk.Treeview(self.__frame, columns=self._columns, show="headings")
+        treeview = self.__render_headings()
+        treeview.grid(row=0, column=0)
+        self.__frame.pack(fill="both", expand=True)
 
-        for i in self._columns:
+    def __render_headings(self):
+        columns = [
+            "Forename",
+            "Surname",
+            "Age",
+            "Tenure",
+            "Role"
+        ]
+
+        treeview = ttk.Treeview(self.__frame, columns=columns, show="headings")
+
+        for i in columns:
             treeview.heading(i, text=i)
 
-        treeview.grid(row=0, column=0)
-
-        self.__frame.pack(fill="both", expand=True)
+        return treeview
 
     def clear(self):
         for widgets in self.__frame.winfo_children():
