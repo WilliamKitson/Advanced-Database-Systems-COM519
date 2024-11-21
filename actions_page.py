6a34f3576358263d711ec6e6c6ffe1c6e81f65a6
4kitsw10 <4kitsw10@solent.ac.uk>
2024-11-21 10:12:29 +0000

    actions_page redefined

--- actions_page.py
+++ actions_page.py
@@ -1,6 +1,10 @@
+from customer_menu_page import CustomerMenuPage
 from treeview_page import TreeviewPage
 from tkinter import *
 from functools import partial
+
+from view_staff_page import ViewStaffPage
+
 
 class ActionsPage(TreeviewPage):
     def __init__(self, database):
@@ -18,7 +22,21 @@ class ActionsPage(TreeviewPage):
     def __render_treeview(self):
         self._treeview.insert("", "end", values=("View Staff", "BLABLABLA"))
         self._treeview.insert("", "end", values=("View Menu", "BLABLABLA"))
+        self._treeview.bind("<Double-1>", self.__execute_action)
         self._treeview.grid(row=0, column=0)
+
+    def __execute_action(self, event):
+        item = self._treeview.selection()[0]
+        if self._treeview.item(item, "values")[0] == "View Staff":
+            self._window.destroy()
+            ViewStaffPage(self._database).render()
+            return
+
+        if self._treeview.item(item, "values")[0] == "View Menu":
+            self._window.destroy()
+            CustomerMenuPage(self._database).render()
+            return
+
 
     def __render_logout(self):
         command_logout = partial(self.__logout)
