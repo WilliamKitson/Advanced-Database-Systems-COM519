5bf0d91afc3e1ec0e5c84efe6912a3051b863269
4kitsw10 <4kitsw10@solent.ac.uk>
2024-11-21 09:46:10 +0000

    main redefined

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-from customer_menu_page import CustomerMenuPage
 from login_page import LoginPage
-from view_staff_page import ViewStaffPage
-
 LoginPage("4kitsw10_COM519_database").render()
-CustomerMenuPage("4kitsw10_COM519_database").render()
-ViewStaffPage("4kitsw10_COM519_database").render()
