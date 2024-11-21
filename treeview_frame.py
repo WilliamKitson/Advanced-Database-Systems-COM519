89e2fd2bc9eb1b2b4cf160f40009d400954e06ad
4kitsw10 <4kitsw10@solent.ac.uk>
2024-11-21 11:55:43 +0000

    treeview_frame redefined

--- treeview_frame.py
+++ treeview_frame.py
@@ -5,9 +5,12 @@ from tkinter import ttk
 class TreeviewFrame:
     def __init__(self, database, window):
         self.__frame = Frame(window)
+        self._cursor = sqlite3.connect(database).cursor()
 
     def render(self):
         treeview = self.__render_headings()
+        self.__render_body(treeview)
+
         treeview.grid(row=0, column=0)
         self.__frame.pack(fill="both", expand=True)
 
@@ -26,6 +29,21 @@ class TreeviewFrame:
             treeview.heading(i, text=i)
 
         return treeview
+
+    def __render_body(self, treeview):
+        self._cursor.execute(
+            "SELECT * "
+            "FROM View_Team_Members"
+        )
+
+        for row in self._cursor.fetchall():
+            treeview.insert("", "end", values=(
+                row[0],
+                row[1],
+                f"{row[2]} years",
+                f"{row[3]} years",
+                row[4]
+            ))
 
     def clear(self):
         for widgets in self.__frame.winfo_children():
