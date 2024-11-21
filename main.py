8ce3e85e2a5788bc102277c7c4507e7aa0739726
4kitsw10 <4kitsw10@solent.ac.uk>
2024-11-21 11:38:56 +0000

    main redefined

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ window = tkinter.Tk()
 window.title("4kitsw10_COM519")
 
 TreeviewFrame("4kitsw10_COM519_database", ["one", "two", "three"]).render(window)
+TreeviewFrame("4kitsw10_COM519_database", ["four", "five", "six"]).render(window)
 
 window.mainloop()
