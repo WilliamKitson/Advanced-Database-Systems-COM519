0bf8ecbf6411b67d4390b57f991fcd5e5c93fded
4kitsw10 <4kitsw10@solent.ac.uk>
2024-11-26 13:09:33 +0000

    presenter redefined

--- presenter.py
+++ presenter.py
@@ -9,19 +9,28 @@ class Presenter:
         self.__window.geometry('400x250')
 
     def render(self):
+        self.__render_login()
+        self.__window.mainloop()
+
+    def __render_login(self):
         username = StringVar()
         password = StringVar()
 
+        self.__render_login_username(username)
+        self.__render_login_password(password)
+        self.__render_login_submit(username, password)
+
+    def __render_login_username(self, username):
         Label(self.__window, text="Username").grid(row=0, column=0)
         Entry(self.__window, textvariable=username).grid(row=0, column=1)
 
+    def __render_login_password(self, password):
         Label(self.__window, text="Password").grid(row=1, column=0)
         Entry(self.__window, textvariable=password, show='*').grid(row=1, column=1)
 
-        command_login = partial(self.__TEMP_login_procedure)
+    def __render_login_submit(self, username, password):
+        command_login = partial(self.__TEMP_login_procedure, username, password)
         Button(self.__window, text="Login", command=command_login).grid(row=3, column=0)
 
-        self.__window.mainloop()
-
-    def __TEMP_login_procedure(self):
-        print("temp login procedure")
+    def __TEMP_login_procedure(self, username, password):
+        print(f"{username.get()}:{password.get()}")
