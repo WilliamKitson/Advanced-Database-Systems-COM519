586c1a1acb2ae41b5d3298661228701fe722962f
4kitsw10 <4kitsw10@solent.ac.uk>
2024-12-07 17:27:27 +0000

    database redefined

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #  Copyright (c) 2024. William E. Kitson
 
 from image_writer import ImageWriter
-ImageWriter("4kitsw10_COM519_database").load("Coffee")
+ImageWriter("4kitsw10_COM519_database").load("Ice Cream")
 
 from presentation import Presentation
 Presentation("4kitsw10_COM519_database").render()
