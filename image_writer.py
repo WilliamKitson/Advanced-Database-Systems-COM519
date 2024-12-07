b62c46c3c1a762e3bb6f77ad3bcd253141662426
4kitsw10 <4kitsw10@solent.ac.uk>
2024-12-07 16:55:35 +0000

    image_writer.py defined

---
+++ image_writer.py
@@ -0,0 +1,27 @@
+#  Copyright (c) 2024. William E. Kitson
+
+import sqlite3
+from tkinter import filedialog
+
+class ImageWriter:
+    def __init__(self, database):
+        self.__database = sqlite3.connect(database)
+        self.__cursor = self.__database.cursor()
+
+    def write(self, menu_id):
+        with open(filedialog.askopenfilename(), 'rb') as file:
+            file_blob = file.read()
+
+        query = (
+            "UPDATE Menu "
+            "SET Image = ? "
+            "WHERE Menu_Id = ?"
+        )
+
+        parameters = (
+            file_blob,
+            menu_id
+        )
+
+        self.__cursor.execute(query, parameters)
+        self.__database.commit()
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 #  Copyright (c) 2024. William E. Kitson
 
+from image_writer import ImageWriter
+ImageWriter("4kitsw10_COM519_database").write(2)
+
 from presentation import Presentation
 Presentation("4kitsw10_COM519_database").render()
