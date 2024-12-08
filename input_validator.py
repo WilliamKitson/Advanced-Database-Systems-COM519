01d0af248f45d024c7ce4755801f9e9b600fb9b8
4kitsw10 <4kitsw10@solent.ac.uk>
2024-12-08 13:17:03 +0000

    input_validator.py redefined

--- input_validator.py
+++ input_validator.py
@@ -8,9 +8,12 @@ class InputValidator:
 
     def valid_password(self, password):
         if self.__validate_password_lowercase(password):
-            return True
+            return False
 
-        return False
+        if self.__validate_password_uppercase(password):
+            return False
+
+        return True
 
     def __validate_password_lowercase(self, password):
         lowercases = 0
@@ -18,4 +21,12 @@ class InputValidator:
         for i in self.__lowercase:
             lowercases += password.find(i) != -1
 
-        return lowercases > 0
+        return lowercases == 0
+
+    def __validate_password_uppercase(self, password):
+        uppercases = 0
+
+        for i in self.__uppercase:
+            uppercases += password.find(i) != -1
+
+        return uppercases == 0
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from image_writer import ImageWriter
 ImageWriter("4kitsw10_COM519_database").load("Ice Cream")
 
 from input_validator import InputValidator
-print(InputValidator().valid_password("QWqERTYUIOPASDFGHJKLZXCVBNM"))
+print(InputValidator().valid_password("qQ"))
 
 from presentation import Presentation
 Presentation("4kitsw10_COM519_database").render()
